eb262db3d1a063add137595fe3ad42484463cb1e
Slava.Rudko <1234567890cdz@gmail.com>
2024-10-28 00:27:03 +0300

    complite HW

--- venv_Kyrsi/lesson_4/HW_4.1.py
+++ venv_Kyrsi/lesson_4/HW_4.1.py
@@ -1,5 +1,6 @@
-python_zen=['The Zen of Python, by Tim Peters
-Beautiful is better than ugly.
+from collections import Counter
+
+python_zen = '''Beautiful is better than ugly.
 Explicit is better than implicit.
 Simple is better than complex.
 Complex is better than complicated.
@@ -17,5 +18,28 @@ Now is better than never.
 Although never is often better than right now.
 If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
-Namespaces are one honking great idea -- let's do more of those!']
+Namespaces are one honking great idea -- let's do more of those!'''
 
+line_count = len(python_zen.split('\n'))
+print(line_count)
+
+split_zen = python_zen.split()
+is_zen = Counter(split_zen)
+and_zen = Counter(split_zen)
+or_zen = Counter(split_zen)
+
+element_is = 'is'
+element_and = 'and'
+element_or = 'or'
+
+count_is = is_zen[element_is]
+count_and = is_zen[element_and]
+count_or = is_zen[element_or]
+
+dict_zen = {element_is:count_is, element_and:count_and, element_or:count_or}
+print(dict_zen)
+
+list_zen = list({python_zen})
+string_zen = " ".join(list_zen)
+new_zen = string_zen.replace("is", "is not")
+print(new_zen)
