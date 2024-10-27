b8e8aab2646fcc33d52f57242613ef4bcc7372fd
Slava.Rudko <1234567890cdz@gmail.com>
2024-10-27 20:06:39 +0300

    add ht-4

--- venv_Kyrsi/lesson_3/HW_3.py
+++ venv_Kyrsi/lesson_3/HW_3.py
@@ -4,16 +4,16 @@ age = input("Введите возраст: ")
 task_1 = f'Привет, меня зовут {firstname} {lastname}, мне {age} лет'
 print(task_1)
 
-task_2 = task_1[3]
+task_2 = task_1[2]
 print(task_2)
 
 task_3 = task_1[-2]
 print(task_3)
 
-task_4 = task_1[0:4]
+task_4 = task_1[0:5]
 print(task_4)
 
-task_5 = task_1[0:-3]
+task_5 = task_1[0:-2]
 print(task_5)
 
 some_str = "Hello, World!"
